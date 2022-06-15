f3afc6e98d43a35a0191c0c4339e0ceb1be7966f
anselmeatchogou <anselmeatchogou@gmail.com>
2022-06-15 17:00:51 +0100

    dashboard designed

--- connectDB.php
+++ connectDB.php
@@ -1,5 +1,5 @@
 <?php
-/* Database connection settings */
+/* Database connection setting */
 	$servername = "mysql-ansoatc.alwaysdata.net";
     $username = "ansoatc";		//put your phpmyadmin username.(default is "root")
     $password = "anso20092001";			//if your phpmyadmin has a password put it here.(default is "root")
